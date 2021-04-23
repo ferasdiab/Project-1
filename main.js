2fb0c477bcb6c2a1412cdc63ed19229d2d87ec23
ferasdiab <dfiras96@gmail.com>
2021-04-23 16:53:54 +0300

    Describe the changes

--- main.js
+++ main.js
@@ -1,55 +1,3 @@
-/*
-
-let q1 = "What is the largest country in the world ?"
-let q2 = "Who Invented the Light Bulb ?"
-let q3 = "What is the longest river in the world ?"
-
-const qu = [{qu : q1  } , {qu : q2 } , {qu : q3 } ]
-
-const miltiOption = [
-{first : "Russia" , second : "China" , third : "USA" , forth : "Canada"},
-{first : "Thomas Edison" , second : "Joseph Swan" , third : "Nikola Tesla" , forth : "Albert Einstein"},
-{first : "Amazon River" , second : "Mississippi River" , third : "Yangtze River" , forth : "Nile river"}
-]
-
-const rightAnswer = ["a","a","a","a"]
-
-let counter = 0
-const newQA =  document.querySelector("#QA");
-const newOption1 =  document.querySelector("#aaaaa");
-const newOption2 =  document.querySelector("#bbbbb");
-const newOption3 =  document.querySelector("#cccccc");
-const newOption4 =  document.querySelector("#dddddd");
-const buttonstats =  document.querySelector("button");
-const chosenanswer =  document.querySelector("#multi1");
-console.log(chosenanswer.value )
-
-
-
-
-
-const nextQA = () => {
-
-newQA.innerHTML = qu[counter].qu;
-newOption1.innerText = miltiOption[counter].first
-newOption2.innerText = miltiOption[counter].second
-newOption3.innerText = miltiOption[counter].third
-newOption4.innerText = miltiOption[counter].forth
-
-console.log(chosenanswer.value )
-
-if ( counter === qu.length-1 ){
-    buttonstats.innerText = "Finish"
-} else {
-    buttonstats.innerText = "Next"
- }
-
-
-++counter
-
-}
-
-*/
 
 
 let q1 = "What is the largest country in the world ?"
@@ -60,9 +8,11 @@ const qu = [{qu : q1 , miltiOption : { first : {name : "Russia" , stats : true} 
  {qu : q2 , miltiOption : {first : {name : "Thomas Edison" , stats : true} , second : {name : "Joseph Swan" , stats : false} , third : {name : "Nikola Tesla" , stats : false} , forth : {name : "Albert Einstein" , stats : false}}  } , 
  {qu : q3  , miltiOption: {first : {name : "Amazon River" , stats : false} , second : {name : "Mississippi River" , stats : false} , third : {name : "Yangtze River" , stats : false} , forth : {name : "Nile river" , stats : true}}} ]
 
-
+ 
 
 let counter = 0
+let theScore = 0
+
 const newQA =  document.querySelector("#QA");
 const newOption1 =  document.querySelector("#aaaaa");
 const newOption2 =  document.querySelector("#bbbbb");
@@ -97,6 +47,8 @@ const nextQA = () => {
 
 if (counter !== qu.length ){
 
+ 
+
 newQA.innerHTML = qu[counter].qu;
 
 newOption1.innerText = qu[counter].miltiOption.first.name;
@@ -109,6 +61,8 @@ option1stats.value = qu[counter].miltiOption.first.stats;
 option2stats.value = qu[counter].miltiOption.second.stats;
 option3stats.value = qu[counter].miltiOption.third.stats;
 option4stats.value = qu[counter].miltiOption.forth.stats;
+
+
 
 console.log( option1stats.value)
 console.log( option2stats.value)
@@ -124,8 +78,14 @@ if ( counter === qu.length-1 ){
  }
 
 } else {
-    body.style.display = "none";
-    score.innerHTML = "full grade"
+    body.remove();
+    score.innerHTML = `${theScore} OF ${qu.length}`
+    document.getElementById("result").style.position = "relative";
+    document.getElementById("result").style.top = "50%";
+    document.getElementById("result").style.width = "350px"
+    document.getElementById("result").style.height = "100px"
+    document.getElementById("result").style.background = "rgb(36, 62, 109)"
+
 
 
 
@@ -133,5 +93,31 @@ if ( counter === qu.length-1 ){
 
 
 ++counter
+
+}
+
+
+
+
+
+const chick = () => {
+
+const arrOfAnswer = [option1stats.value , option2stats.value
+, option3stats.value  , option4stats.value]
+
+if ((document.getElementsByName("a")[0].checked) === true &&
+arrOfAnswer[0] === "true"  ) { ++theScore }
+
+if ((document.getElementsByName("a")[1].checked) === true &&
+arrOfAnswer[1] === "true"  ) { ++theScore }
+
+if ((document.getElementsByName("a")[2].checked) === true &&
+arrOfAnswer[2] === "true"  ) { ++theScore }
+
+if ((document.getElementsByName("a")[3].checked) === true &&
+arrOfAnswer[3] === "true"  ) { ++theScore }
+
+
+console.log(theScore)
 
 }
